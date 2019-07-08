c86276f57da034663338b9e4a6a482edffc41f48
kierepkae <kierepkaedyta@gmail.com>
2019-07-08 10:45:32 +0100

    created save_students method in directory.rb

--- directory.rb
+++ directory.rb
@@ -14,6 +14,18 @@ def input_students
   end
 end
 
+def save_students
+  #open file for writing
+  file = File.open("students.csv","w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def interactive_menu
   students = []
   loop do
